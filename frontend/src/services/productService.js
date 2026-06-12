af17e481d129bdad00bec276eba5b99905fc0f2c
iamsujithsingh-06 <sujithsinghsm@gmail.com>
2026-06-12 15:57:16 +0530

    fix vercel routes

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,9 +1,9 @@
 import api from './api';
 
 export const productService = {
-  getAll: (params) => api.get('/api/products', { params }),
-  getBySlug: (slug) => api.get(`/api/products/${slug}`),
-  getRelated: (slug) => api.get(`/api/products/${slug}/related`),
-  getFeatured: () => api.get('/api/products/featured'),
-  getFilters: () => api.get('/api/products/filters'),
+  getAll: (params) => api.get('/products', { params }),
+  getBySlug: (slug) => api.get(`/products/${slug}`),
+  getRelated: (slug) => api.get(`/products/${slug}/related`),
+  getFeatured: () => api.get('/products/featured'),
+  getFilters: () => api.get('/products/filters'),
 };
